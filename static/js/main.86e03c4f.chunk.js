976e5a7f5801a1f9162fc4b6e8314849a88b84b8
shaquibimdad <shaquibxyz@gmail.com>
2021-08-13 23:25:19 +0530

    Updates

---
+++ static/js/main.86e03c4f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdaemon=this.webpackJsonpdaemon||[]).push([[0],{10:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),s=t(3),i=t.n(s),o=(t(8),t(0));var a=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"HELLO I AM SHAQUIB IMDAD"})})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))},8:function(e,n,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.86e03c4f.chunk.js.map
--- static/js/main.b029e99f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpdaemon=this.webpackJsonpdaemon||[]).push([[0],{10:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),s=t(3),i=t.n(s),o=(t(8),t(0));var a=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"HELLO I AM SHAQUIB"})})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))},8:function(e,n,t){}},[[10,1,2]]]);
-//# sourceMappingURL=main.b029e99f.chunk.js.map
