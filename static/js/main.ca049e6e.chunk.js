5c6b516511c41f4af1fda4ddd664f4c8e0b2a7b2
shaquibimdad <shaquibxyz@gmail.com>
2021-08-13 23:40:12 +0530

    Updates

---
+++ static/js/main.ca049e6e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdaemon=this.webpackJsonpdaemon||[]).push([[0],{10:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),s=t(3),i=t.n(s),o=(t(8),t(0));var a=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"HELLO I AM SHAQUIB IMDAD AND THIS IS REACT"})})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))},8:function(e,n,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.ca049e6e.chunk.js.map
